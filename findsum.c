cecebbb3ed612e95ab2c68c24c6e9817a8ab6e71
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-02 02:53:28 -0800

    c pgm
    good

---
+++ findsum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+
+printf("Enter the two numbers add\n");
+scanf("%d%d",&a,&b);
+
+c=a+b;
+printf("sum of numbers=%d\n",c);
+
+return 0;
+}
